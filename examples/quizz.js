362e6d393f43606319204e793e4491d4412c51fa
hongarc <aqua.hong.97@gmail.com>
2020-02-14 19:39:02 +0700

    docs: example create quizz

---
+++ examples/quizz.js
@@ -0,0 +1,20 @@
+const Sudoku = require('..');
+
+const grid = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const sudoku = new Sudoku(grid);
+
+const start = Date.now();
+sudoku.quizz();
+console.log(sudoku.code());
+console.log('Generate quizz in', Date.now() - start, 'ms\n');
